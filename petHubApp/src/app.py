f163cc1ef5604f46fd3307a42d378357c3c55ff4
Maiara Trevisol <maiara.trevisol@sap.com>
2024-01-07 17:35:45 -0300

    implementação login e cadastro

--- petHubApp/src/app.py
+++ petHubApp/src/app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from decouple import config
 import pyrebase
 from json import dumps
+import firebase_admin
+from firebase_admin import auth, db
+import json
+import uuid 
+import hashlib
+from pprint import pprint
 
+import firebase_admin
+from firebase_admin import db, credentials
 
 app = Flask(__name__,template_folder='view')
 app.secret_key = 'secret'
@@ -18,57 +26,141 @@ firebaseConfig =  {
     'measurementId': config("FIREBASE_MEASUREMENT_ID"),
 }
 
+#initialize DB
+dbCredentials = {
+  "type": config("TYPE"),
+  "project_id": config("FIREBASE_PROJECT_ID"),
+  "private_key_id": config("PRIVATE_KEY_ID"),
+  "private_key": config("PRIVATE_KEY"),
+  "client_id": config("CLIENT_ID"),
+  "auth_uri": config("AUTH_URI"),
+  "token_uri": config("TOKEN_URI"),
+  "auth_provider_x509_cert_url": config("AUTH_PROVIDER_CERT"),
+  "client_x509_cert_url": config("CLIENT_CERT"),
+  "universe_domain": config("UNIVERSE_DOMAIN"),
+  "client_email": config("CLIENT_EMAIL") 
+}
+cred = credentials.Certificate(dbCredentials)
+firebase_admin.initialize_app(cred , {"databaseURL": "https://pet-hub-rs-default-rtdb.firebaseio.com"})
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth=firebase.auth()
+# creating reference to root node
+ref = db.reference("/")
+users = db.reference("/users")
+
 
 @app.route("/", methods =['POST', 'GET'])
 def index():
     return render_template('index.html')
 
+@app.route("/user", methods =['POST', 'GET'])
+def user():
+    flash(session['user'], "user_name")
+    return render_template('perfil.html')
+
+
 @app.route("/login", methods =['POST', 'GET'])
 def login():
 
     if('user' in session):
-      return 'Hi, {}'.format(session['user'])
+      return redirect('/user')
     
     if request.method == 'POST':
-        email = request.form.get('email')
+        document = request.form.get('document')
         password = request.form.get('password')
         try:
-            user = auth.sign_in_with_email_and_password(email, password)
-            print(user)
-            session['user'] = email
-            return redirect('/login')
-        except:
-            return 'Failed to login'
+            document_formatted = document.replace("-", "").replace(".", "").replace("/", "")
+            loginUser = users.child(document_formatted).get()
+
+            #Hash that Passsword
+            password_encoded = password.encode('utf-8')
+
+            # Create a SHA-1 hash object
+            sha1 = hashlib.sha1()
+
+            # Update the hash object with the encoded password
+            sha1.update(password_encoded)
+
+            # Get the hexadecimal representation of the hash
+            hashed_password = sha1.hexdigest()
         
-    return render_template('login.html')
+            if loginUser['userPassword'] == hashed_password:
+                session['user'] = loginUser['userName']
+                return redirect('/user')
+        except:
+            return render_template('login.html')
+    if request.method == "GET":
+        return render_template('login.html')
+        
+
+def is_document_number_unique(document_number):
+    query =  users.child(document_number).get()
+    return True if query == None else False
+ 
+    
+def invalid_password_or_document(password_encoded, password_confirmation_encoded, document_number):
+    wrong_password = False
+    unique_document = True
+    if  password_encoded != password_confirmation_encoded:
+        flash("as senhas escolhidas divergem", "different_passwords_message")
+        wrong_password = True
+            
+    if not is_document_number_unique(document_number):
+        flash("documento já cadastrado", "already_existing_document")
+        unique_document = False
+        
+    if  wrong_password or not unique_document:
+        return True
+        
 
 @app.route("/cadastro", methods =['POST', 'GET'])
 def cadastro():
     if request.method == 'POST':
-        # Obtenha os dados do formulário
+        # generates Random UID for Database
+        idx = uuid.uuid4()
+        uid = str(idx)
         email = request.form.get('email')
-        password = request.form.get('password')
-        display_name = request.form.get('display_name')
+        name = request.form.get('nome')
+        document = request.form.get('cpfOuCnpj')
+        document_formatted = document.replace("-", "").replace(".", "").replace("/", "")
+        password = request.form.get('password1')
+        password_confirmation = request.form.get('password2')
+        # TODO: teremos profile picture?
+        # profilePicture = request.form.get('profilePicture')
+        
+        #Hash that Passsword
+        password_encoded = password.encode('utf-8')
+        password_confirmation_encoded = password_confirmation.encode('utf-8')
 
-        try:
-            # Crie um novo usuário no Firebase Authentication
-            # user = auth.create_user(
-            #     email=email,
-            #     password=password,
-            #     display_name=display_name
-            # )
+        if invalid_password_or_document(password_encoded, password_confirmation_encoded, document_formatted):
+            return redirect('/cadastro')
+        else:
 
-            # Realize ações adicionais conforme necessário
+            # Create a SHA-1 hash object
+            sha1 = hashlib.sha1()
 
-            return dumps(user)
+            # Update the hash object with the encoded password
+            sha1.update(password_encoded)
 
-        except Exception as e:
-            return dumps(str(e))
-
+            # Get the hexadecimal representation of the hash
+            hashed_password = sha1.hexdigest()
+            
+            #Database Directive
+            users.child(document_formatted).set(
+            {
+            'uid': uid,
+            'userName': name,
+            'userPassword':  hashed_password,
+            'userEmail': email,
+            # 'profilePicture': profilePicture
+            }
+            )
+            return redirect('/login')
     return render_template('cadastro.html')
+
+@app.route("/logout", methods =['GET'])
+def logout():
+    session.pop('user')
+    return redirect("/")
 
 
 
